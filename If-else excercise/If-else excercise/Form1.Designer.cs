254dc45a26cf800e15bfa5a4a87e7d4f2dff4213
FatMeryl <Fatmeryl>
2021-03-27 18:55:38 +0100

    If Else excercise

---
+++ If-else excercise/If-else excercise/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace If_else_excercise
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.changeText = new System.Windows.Forms.Button();
+            this.enableCheckbox = new System.Windows.Forms.CheckBox();
+            this.labelToChange = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // changeText
+            // 
+            this.changeText.Location = new System.Drawing.Point(24, 40);
+            this.changeText.Name = "changeText";
+            this.changeText.Size = new System.Drawing.Size(130, 41);
+            this.changeText.TabIndex = 0;
+            this.changeText.Text = "Click for change text";
+            this.changeText.UseVisualStyleBackColor = true;
+            this.changeText.Click += new System.EventHandler(this.changeText_Click);
+            // 
+            // enableCheckbox
+            // 
+            this.enableCheckbox.AutoSize = true;
+            this.enableCheckbox.Checked = true;
+            this.enableCheckbox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.enableCheckbox.Location = new System.Drawing.Point(215, 53);
+            this.enableCheckbox.Name = "enableCheckbox";
+            this.enableCheckbox.Size = new System.Drawing.Size(118, 17);
+            this.enableCheckbox.TabIndex = 1;
+            this.enableCheckbox.Text = "turn on change text";
+            this.enableCheckbox.UseVisualStyleBackColor = true;
+            // 
+            // labelToChange
+            // 
+            this.labelToChange.AutoSize = true;
+            this.labelToChange.Font = new System.Drawing.Font("Microsoft Tai Le", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelToChange.Location = new System.Drawing.Point(75, 135);
+            this.labelToChange.Name = "labelToChange";
+            this.labelToChange.Size = new System.Drawing.Size(199, 16);
+            this.labelToChange.TabIndex = 2;
+            this.labelToChange.Text = "Click the button to change text";
+            this.labelToChange.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 194);
+            this.Controls.Add(this.labelToChange);
+            this.Controls.Add(this.enableCheckbox);
+            this.Controls.Add(this.changeText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button changeText;
+        private System.Windows.Forms.CheckBox enableCheckbox;
+        private System.Windows.Forms.Label labelToChange;
+    }
+}
+
---
+++ If-else excercise/If-else excercise/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace If_else_excercise
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void changeText_Click(object sender, EventArgs e)
+        {
+            if (!enableCheckbox.Checked)
+            {
+                labelToChange.Text = "Can not change text";
+                labelToChange.Location = new Point(75,135);
+            }
+        }
+    }
+}
